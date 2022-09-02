a743cc46cc34c587e0e0ee0a50d9b2e6fae12c03
Md. Bodiuzzaman Ripon <riponzamanbd@gmail.com>
2022-09-02 21:01:16 +0600

    first commit

---
+++ database/migrations/2022_09_02_145442_create_students_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('students', function (Blueprint $table) {
+            $table->id();
+            $table->String('name',50);
+            $table->String('roll',50);
+            $table->String('city',50);
+            $table->String('phone',50);
+            $table->text('description');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('students');
+    }
+};
